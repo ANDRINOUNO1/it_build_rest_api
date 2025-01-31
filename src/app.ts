aab741900186e31eded806349805d3b40c6302d0
ANDRINOUNO1 <mataandrewczar@gmail.com>
2025-01-31 18:09:16 +0800

    secondg commit

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import express from "express"
 import * as dotenvv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
-import { userRouter } from "./users/user.routes";
-
+import { userRouter } from "./users/users.routes"
+import { productRouter } from "./products/product.routes"
 
 dotenvv.config()
 
@@ -20,7 +20,8 @@ app.use(express.urlencoded({extended : true}))
 app.use(cors())
 app.use(helmet())
 
-app.use('/',userRouter)
+app.use('/', userRouter)
+app.use('/', productRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
